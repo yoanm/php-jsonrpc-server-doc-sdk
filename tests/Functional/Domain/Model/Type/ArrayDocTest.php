3d8e564e47c594b105841227a81db5d7bf140a92
Yoanm <yoanm@users.noreply.github.com>
2018-05-20 14:26:34 +0200

    ArrayDoc TT/FT

---
+++ tests/Functional/Domain/Model/Type/ArrayDocTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Tests\Functional\Domain\Model;
+
+use PHPUnit\Framework\TestCase;
+use Yoanm\JsonRpcServerDoc\Domain\Model\Type\ArrayDoc;
+use Yoanm\JsonRpcServerDoc\Domain\Model\Type\BooleanDoc;
+
+/**
+ * @covers \Yoanm\JsonRpcServerDoc\Domain\Model\Type\ArrayDoc
+ *
+ * @group Models
+ */
+class ArrayDocTest extends TestCase
+{
+    public function testShouldManageAnItemValidationNullByDefault()
+    {
+        $itemValidation = new BooleanDoc();
+        $doc = new ArrayDoc();
+        $this->assertNull($doc->getItemValidation());
+        $doc->setItemValidation($itemValidation);
+        $this->assertSame($itemValidation, $doc->getItemValidation());
+    }
+}
